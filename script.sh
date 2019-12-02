4466c051930a4b4c560c03ed5a98814860b48d9b
P3TERX <p3terx@outlook.com>
2019-12-02 10:41:36 +0800

    If '/tmp/keepalive' exists, then hide tips

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ echo ___________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
-echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
+[ ! -f /tmp/keepalive ] && echo -e "After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout"
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   MSG=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')

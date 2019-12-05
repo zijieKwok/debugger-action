e7b144a4fc1cb25402404b29d1db809a5d8ba4fa
P3TERX <p3terx@outlook.com>
2019-12-05 22:22:19 +0800

    Display tmate web terminal.

--- script.sh
+++ script.sh
@@ -29,6 +29,8 @@ echo ___________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+echo To connect to this session copy-n-paste the following into a browser:
+tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
 [ ! -f /tmp/keepalive ] && echo -e "After connecting you can run 'touch /tmp/keepalive' to disable the 30m timeout"
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then

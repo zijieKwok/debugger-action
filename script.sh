3000ffcba98f9353dc4beb776172aaf7993256e5
Texot <tete1030@gmail.com>
2020-01-06 21:16:56 +0800

    Automatic detect connection

--- script.sh
+++ script.sh
@@ -5,12 +5,9 @@ set -eo pipefail
 # For mount docker volume, do not directly use '/tmp' as the dir
 TMATE_TERM=${TMATE_TERM:-screen-256color}
 TIMESTAMP="$(date +%s)"
-MILLISEC="$(date +%3N)"
-TIMESTAMP_MILLISEC="${TIMESTAMP}${MILLISEC}"
-TMATE_SOCK_FILE="/tmp/tmate-${TIMESTAMP_MILLISEC}.sock"
-KEEPALIVE_DIR="/tmp/tmate-${TIMESTAMP}"
-KEEPALIVE_FILE="${KEEPALIVE_DIR}/keepalive"
-TMATE_SESSION_NAME="tmate-${TIMESTAMP_MILLISEC}"
+TMATE_DIR="/tmp/tmate-${TIMESTAMP}"
+TMATE_SOCK="${TMATE_DIR}/session.sock"
+TMATE_SESSION_NAME="tmate-${TIMESTAMP}"
 
 cleanup() {
   if [ -n "${container_id}" -a -n "${TMATE_DOCKER_IMAGE}" ]; then
@@ -19,10 +16,9 @@ cleanup() {
     docker commit --message "Commit from debugger-action" "${container_id}" "${TMATE_DOCKER_IMAGE_EXP}"
     docker rm "${container_id}" > /dev/null
   fi
-  tmate -S ${TMATE_SOCK_FILE} kill-server || true
+  tmate -S "${TMATE_SOCK}" kill-server || true
   sed -i '/alias attach_docker/d' ~/.bashrc || true
-  rm -rf "${KEEPALIVE_DIR}"
-  rm -f "${TMATE_SOCK_FILE}"
+  rm -rf "${TMATE_DIR}"
 }
 
 if [[ -n "$SKIP_DEBUGGER" ]]; then
@@ -56,9 +52,9 @@ now_date="$(date)"
 timeout=$(( ${TIMEOUT_MIN:=30}*60 ))
 kill_date="$(date -d "${now_date} + ${timeout} seconds")"
 
-mkdir "${KEEPALIVE_DIR}" || true
-chmod 777 "${KEEPALIVE_DIR}"
-rm "${KEEPALIVE_FILE}" || true
+TMATE_SESSION_PATH="$(pwd)"
+mkdir "${TMATE_DIR}"
+
 container_id=''
 if [ -n "${TMATE_DOCKER_IMAGE}" -o -n "${TMATE_DOCKER_CONTAINER}" ]; then
   if [ -n "${TMATE_DOCKER_CONTAINER}" ]; then
@@ -68,32 +64,36 @@ if [ -n "${TMATE_DOCKER_IMAGE}" -o -n "${TMATE_DOCKER_CONTAINER}" ]; then
       TMATE_DOCKER_IMAGE_EXP="${TMATE_DOCKER_IMAGE}"
     fi
     echo "Creating docker container for running tmate"
-    container_id=$(docker create -t -v "${KEEPALIVE_DIR}:${KEEPALIVE_DIR}" "${TMATE_DOCKER_IMAGE}")
+    container_id=$(docker create -t "${TMATE_DOCKER_IMAGE}")
     docker start "${container_id}"
   fi
-  docker exec -i -u root "${container_id}" rm "${KEEPALIVE_FILE}" || true
   DK_SHELL="docker exec -e TERM='${TMATE_TERM}' -it ${container_id} /bin/bash -il"
-  DOCKER_MESSAGE_CMD='printf "This window is running in Docker image.\nTo attach to Github Actions runner, exit current shell\nor create a new tmate window by \"Ctrl-b, c\"\n(This shortcut is only available when connecting through ssh)\nAfter connecting you should run \`touch '"${KEEPALIVE_FILE}"'\` to disable the timeout.\nOr the session will be KILLED in '"${timeout}"' seconds at '"${kill_date}"'\n\n"'
+  DOCKER_MESSAGE_CMD='printf "This window is running in Docker image.\nTo attach to Github Actions runner, exit current shell\nor create a new tmate window by \"Ctrl-b, c\"\n(This shortcut is only available when connecting through ssh)\n\n"'
   FIRSTWIN_MESSAGE_CMD='printf "This window is now running in GitHub Actions runner.\nTo attach to your Docker image again, use \"attach_docker\" command\n\n"'
   SECWIN_MESSAGE_CMD='printf "The first window of tmate has already been attached to your Docker image.\nThis window is running in GitHub Actions runner.\nTo attach to your Docker image again, use \"attach_docker\" command\n\n"'
   echo "unalias attach_docker 2>/dev/null || true ; alias attach_docker='${DK_SHELL}'" >> ~/.bashrc
-  TERM="${TMATE_TERM}" tmate -S ${TMATE_SOCK_FILE} new-session -s ${TMATE_SESSION_NAME} -d "/bin/bash --noprofile --norc -c '${DOCKER_MESSAGE_CMD} ; ${DK_SHELL} ; ${FIRSTWIN_MESSAGE_CMD} ; /bin/bash -li'" \; set-option default-command "/bin/bash --noprofile --norc -c '${SECWIN_MESSAGE_CMD} ; /bin/bash -li'" \; set-option default-terminal "${TMATE_TERM}"
+  (
+    cd "${TMATE_DIR}"
+    TERM="${TMATE_TERM}" tmate -S "${TMATE_SOCK}" new-session -s ${TMATE_SESSION_NAME} -c "${TMATE_SESSION_PATH}" -d "/bin/bash --noprofile --norc -c '${DOCKER_MESSAGE_CMD} ; ${DK_SHELL} ; ${FIRSTWIN_MESSAGE_CMD} ; /bin/bash -li'" \; set-option default-command "/bin/bash --noprofile --norc -c '${SECWIN_MESSAGE_CMD} ; /bin/bash -li'" \; set-option default-terminal "${TMATE_TERM}"
+  )
 else
-  MESSAGE_CMD='printf "After connecting you should run \`touch '"${KEEPALIVE_FILE}"'\` to disable the timeout.\nOr the session will be KILLED in '"${timeout}"' seconds at '"${kill_date}"'\n\n"'
   echo "unalias attach_docker 2>/dev/null || true" >> ~/.bashrc
-  TERM="${TMATE_TERM}" tmate -S ${TMATE_SOCK_FILE} new-session -s ${TMATE_SESSION_NAME} -d "/bin/bash --noprofile --norc -c '${MESSAGE_CMD} ; /bin/bash -li'" \; set-option default-terminal "${TMATE_TERM}"
+  (
+    cd "${TMATE_DIR}"
+    TERM="${TMATE_TERM}" tmate -S "${TMATE_SOCK}" new-session -s ${TMATE_SESSION_NAME} -c "${TMATE_SESSION_PATH}" -d \; set-option default-terminal "${TMATE_TERM}"
+  )
 fi
 
-tmate -S ${TMATE_SOCK_FILE} wait tmate-ready
-
-SSH_LINE="$(tmate -S ${TMATE_SOCK_FILE} display -p '#{tmate_ssh}')"
-WEB_LINE="$(tmate -S ${TMATE_SOCK_FILE} display -p '#{tmate_web}')"
-KEEPALIVE_MESSAGE="After connecting you should run \`touch ${KEEPALIVE_FILE}\` to disable the timeout. Or the session will be *KILLED* in ${timeout} seconds at ${kill_date}. To skip this step, simply connect the ssh and exit."
+tmate -S "${TMATE_SOCK}" wait tmate-ready
+TMATE_PID="$(tmate -S "${TMATE_SOCK}" display -p '#{pid}')"
+SSH_LINE="$(tmate -S "${TMATE_SOCK}" display -p '#{tmate_ssh}')"
+WEB_LINE="$(tmate -S "${TMATE_SOCK}" display -p '#{tmate_web}')"
+TIMEOUT_MESSAGE="If you don't connect to this session, it will be *KILLED* in ${timeout} seconds at ${kill_date}. To skip this step, simply connect the ssh and exit."
 
 if [[ -n "$SLACK_WEBHOOK_URL" ]]; then
   MSG="SSH: ${SSH_LINE}\nWEB: ${WEB_LINE}"
   echo -n "Sending information to Slack......"
-  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"\`\`\`\n$MSG\n\`\`\`\n${KEEPALIVE_MESSAGE}\"}" "$SLACK_WEBHOOK_URL"
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"\`\`\`\n$MSG\n\`\`\`\n${TIMEOUT_MESSAGE}\"}" "$SLACK_WEBHOOK_URL"
   printf "\n"
 fi
 
@@ -105,8 +105,15 @@ printf "\n"
 display_int=${DISP_INTERVAL_SEC:=30}
 timecounter=0
 
-while [ -S ${TMATE_SOCK_FILE} ]; do
-  if [ ! -f "${KEEPALIVE_FILE}" ]; then
+user_connected=0
+while [ -S "${TMATE_SOCK}" ]; do
+  connected=0
+  grep -qE '^[[:digit:]\.]+ A mate has joined' "${TMATE_DIR}/tmate-server-${TMATE_PID}.log" && connected=1
+  if [ ${connected} -eq 1 -a ${user_connected} -eq 0 ]; then
+    echo "You just connected! Timeout is now disabled."
+    user_connected=1
+  fi
+  if [ ${user_connected} -ne 1 ]; then
     if (( timecounter > timeout )); then
       echo Waiting on tmate connection timed out!
       cleanup
@@ -131,11 +138,7 @@ while [ -S ${TMATE_SOCK_FILE} ]; do
       echo "The tmate SSH and URL are only sent to your Slack through SLACK_WEBHOOK_URL"
       echo "For detail, refer to https://github.com/tete103%30/debugger-action/blob/master/README.md"
     fi
-    [ ! -f "${KEEPALIVE_FILE}" ] && printf "\nAfter connecting you should run '\e[32mtouch ${KEEPALIVE_FILE}\e[0m' to disable the timeout.\nOr the session will be \e[31mKILLED\e[0m in $(( $timeout-$timecounter )) seconds at ${kill_date}\nTo skip this step, simply connect the ssh and exit.\n"
-    echo "List clients:"
-    tmate -S ${TMATE_SOCK_FILE} list-clients
-    echo "Show messages:"
-    tmate -S ${TMATE_SOCK_FILE} show-messages || true
+    [ "x${user_connected}" != "x1" ] && printf "If you don't connect to this session, it will be \e[31mKILLED\e[0m in $(( $timeout-$timecounter )) seconds at ${kill_date}\nTo skip this step, simply connect the ssh and exit.\n"
     echo ______________________________________________________________________________________________
   fi
 
